f2d1477fca39cd0fef0fef586cef1dd66d35f1fb
daejung2617-hue <daejung2617@dge.go.kr>
2026-03-31 10:29:39 +0900

    Add files via upload

---
+++ S0302.java
@@ -0,0 +1,10 @@
+package jump2java;
+
+public class S0302 {
+	public static void main(String[] args) {
+		String a= "Hello java";
+		String b = "Hellojava";
+		System.out.println(a.equals(b));
+		System.out.println(a.indexOf("a"));
+	}
+}
