6f2740b870f888fc1276b464cf16c7bc80de47b9
daejung2617-hue <daejung2617@dge.go.kr>
2026-06-02 10:23:18 +0900

    Add files via upload

--- S0602A.java
+++ S0602A.java
@@ -4,6 +4,7 @@ class Student{
 	String name;
 	int age;
 	int score;
+	String grade;
 	Student(String name){
 		this.name = name;
 		
@@ -12,11 +13,26 @@ class Student{
 		this.name = name;
 		this.age = age;
 		this.score = 100;
+		
 	}
 	
 	void info() {
 		System.out.println(this.name);
 		System.out.println(this.score);
+	}
+	
+	String setGrade(int score) {
+		this.score =score;
+		if (score>=90) {
+			this.grade = "A";
+		} else if(score >= 70) {
+			this.grade = "B";
+		}  else if(score >= 50) {
+			this.grade = "C";
+		} else {
+			this.grade = "F";
+		}
+		return grade;
 	}
 }
 
@@ -27,11 +43,25 @@ public class S0602A {
 		
 		String name = sc.next();
 		sc.close();
+		String grade;
 		Student s1 = new Student(name);
 		s1.info();
+		System.out.println("등급: "+s1.setGrade(100));
+		grade = s1.setGrade(100);
+		System.out.println("등급: "+ grade);
+		System.out.println("등급: "+ s1.grade);
 		
 		Student s2 = new Student(name,18);
 		s2.info();
+		s2.setGrade(55);
+		
+		Student[] st = new Student[3];
+		st[0] = new Student("철수",12);
+		st[1] = new Student("영희",13);
+		st[2] = new Student("스마트",18);
+		st[0].info();
+		st[1].info();
+		
 	}
 
 }
