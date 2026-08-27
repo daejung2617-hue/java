aa6cb1cbbfedb0205e6225dc3db18d07bc3d846a
daejung2617-hue <daejung2617@dge.go.kr>
2026-08-27 11:14:55 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,67 +1,114 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace _2026_08_26
+namespace _2026_08_27
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //string name;
-            //name = "hello";
-            //Console.WriteLine(name);
-            //string ret = Console.ReadLine();
-            //Console.WriteLine(name + ret);
-            //string strdata = "정말로 반가워!! 행복한 하루";
+            Cast.CastInt();
+            objectType.Boxing();
+            Statments.Ternary();
+            Statments.LoopWhile();
+            Statments.LoopFor();
+            Statments.Control();
+        }
+    }
+    internal class Cast
+    {
+        public static void CastInt()
+        {
+            string val = "33";
+            string val1 = "3.14";
 
-            //Console.WriteLine(strdata.IndexOf("반"));
-            //Console.WriteLine(strdata.IndexOf("행복"));
-            //Console.WriteLine(strdata.Contains("!!"));
-            //Console.WriteLine(strdata.Contains("하루들"));
+            int ret = 0;
+            float ret1 = 0;
 
-            //strdata = strdata.Replace("행복한", "즐거운");
-            //Console.WriteLine(strdata);
+            ret = int.Parse(val);
+            Console.WriteLine("정수값" + ret);
+            ret1 = float.Parse(val);
+            Console.WriteLine("실수값1" + ret);
+            double ret2 = double.Parse(val1);
+            Console.WriteLine("실수값2" + ret2);
 
-            //string strdata2 = "happy Day. Good!!";
-            //Console.WriteLine(strdata2.ToUpper());
-            //Console.WriteLine(strdata2.ToLower());
-            //Console.WriteLine(strdata2.Insert(5, "hi"));
-            //Console.WriteLine(strdata2.Remove(2,3));
-            //Console.WriteLine(strdata2.Remove(7));
-            Cast.CastString();
+
+            Console.WriteLine("");
+            ret = Convert.ToInt32(val);
+            ret1 =  Convert.ToSingle(val1);
+            Console.WriteLine("결과{0:F4}", ret1);
 
         }
     }
-    internal class Cast 
+    internal class objectType
     {
-        public static void CastString()
+        public static void Boxing()
         {
-            float num1 = 3.14159f;
-            int retvalue = 0;
+            object box = 50;
+            Console.WriteLine("박싱값:{0}", box);
 
-            Console.WriteLine(num1);
-            Console.WriteLine(retvalue);
+            int unboxed = (int)box;
+            Console.WriteLine("언박싱 값: {0}",unboxed);
 
-            retvalue = (int)(num1);
-            Console.WriteLine(num1);
-            Console.WriteLine(retvalue);
+            unboxed += 5;
+            Console.WriteLine("언박싱 계산 결과: {0}", unboxed);
+        }
+    }
+    internal class Statments
+    {
+        public static void Ternary()
+        {
+            
+            string num = Console.ReadLine();
+            int num1 = int.Parse(num);
+            int abs;
 
-            num1 = (float)retvalue;
-            Console.WriteLine(num1);
+            abs = (num1 > 0) ? num1 : num1 * (-1);
+            Console.WriteLine(num + "절대값=" + abs);
+        }
+        public static void LoopWhile() 
+        {
+            int count = 0;
+            Console.WriteLine("현재 카운트값:{0}",count);
+            while (count > 5)
+            {
+                Console.WriteLine("반복" + count);
+                count++;
+            }
+            Console.WriteLine("반복 완료");
+        }
+        public static void LoopFor() 
+        {
+            Console.WriteLine("출력하고자 하느 구구단의 단:");
+            string input_str = Console.ReadLine();
+            int inputdan = int.Parse(input_str);
 
-            int num = 33;
-            string val1;
-            val1 = "" + num;
-            Console.WriteLine(val1);
-            num = 77;
-            val1 = num.ToString();
-            Console.WriteLine(val1);
-            float num2 = 3.14159f;
-            val1 = string.Format("{0} {1:F2}",num,num2);
-            Console.WriteLine("{0}",val1);
+            for(int i =1; i<=9; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", inputdan, i, i * inputdan);
+            }
+            Console.WriteLine("");
+            for (int i = 2; i <=9; i++)
+            {
+                Console.WriteLine("{0}단 출력",i);
+                for (int j =1;  j<=9; j++)
+                {
+                    Console.WriteLine("{0} * {1} ={2}", i, j, i * j);
+                }
+                Console.WriteLine("");
+            }
+        }
+        public static void Control()
+        {
+            int i = 0;
+            for (i=0; i < 10; i++)
+            {
+                if (i == 8)
+                    break;
+                if (i == 3)
+                    continue;
+                Console.WriteLine(i);
+            }
         }
     }
 }
