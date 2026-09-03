6bdec37669cbfd1d90ab78494ff2aee4de653392
daejung2617-hue <daejung2617@dge.go.kr>
2026-09-03 09:25:26 +0900

    Add files via upload

---
+++ 2026_09_03.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace gameProject
+{
+    internal class Program
+    {
+        static void showData()
+        {
+            Console.WriteLine("외부데이터 없이 자체 동작");
+        }
+
+        static int devide(int x, int y)
+        {
+            if (y != 0)
+            {
+                int retval;
+                retval = x/y;
+                Console.WriteLine("{0} {1}을 나눈값 {2}", x, y, retval);
+            }
+            else
+            {
+                Console.WriteLine("0으로 나눌 수 없음");
+                return 0;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            showData();
+            devide(7, 3);
+            devide(0, 3);
+
+        }
+    }
+}
