c2b350ebf9c02135429f91476fb128acc4ef00c4
daejung2617-hue <daejung2617@dge.go.kr>
2026-09-03 11:56:31 +0900

    Add files via upload

---
+++ 20260903.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Array
+{
+    internal class Program
+    {
+        struct UserData1
+        {
+            public int Id;
+            public float point;
+            public void ShowData()
+            {
+                Console.WriteLine("ID" + Id + "점수: " + point);
+            }
+        }
+        struct UserData2
+        {
+            int age;
+            public void ShowData()
+            {
+                Console.WriteLine("나이: "+ age);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] scores;
+            scores = new int[5];
+            scores[1] = 5;
+            Console.WriteLine(scores[0]);
+            for(int i=0; i<scores.Length; i++)
+            {
+                Console.WriteLine(scores[i]);
+            }
+            float[] data_float;
+            data_float = new float[3] {1.1f,1.2f,1.3f};
+
+            string[] data_str = new string[3] { "aaa", "bbb", "ccc" };
+            for(int i=0; i<data_float.Count(); i++)
+            {
+                Console.Write(data_float[i] + data_str[i]);
+            }
+            foreach (string v in data_str)
+            {
+                Console.WriteLine(v);
+            }
+            Console.WriteLine("구조체 사용");
+            UserData1 mydata1;
+            mydata1.Id = 1;
+            mydata1.point = 2.11f;
+            mydata1.ShowData();
+
+            UserData2 mydata2 = new UserData2();
+            mydata2.ShowData();
+            
+        }
+    }
+}
